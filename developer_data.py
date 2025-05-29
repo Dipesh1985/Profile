b543d2ddb8369ac27f1c6e240bca08d0d7b6c395
Dipesh1985 <tiwaridipesh739@gmail.com>
2025-05-29 14:26:24 +0530

    Add files via upload

---
+++ developer_data.py
@@ -0,0 +1,9 @@
+import streamlit as st
+col1,col2=st.columns([0.5,0.4])
+with col1:
+    st.image("Untitled-1.png")
+
+st.header("Dipesh Mani Tiwari")
+st.write("**Nationality:**  Indian")
+st.write("**Experinced:** Python ,Html")
+st.write("**Email:** @tiwaridipesh739@gmail.com")
