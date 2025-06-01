dedf2bb27bae217da55b4859e4f2b4fe49bc9f48
Dipesh1985 <tiwaridipesh739@gmail.com>
2025-06-01 14:44:07 +0530

    Update developer_data.py

--- developer_data.py
+++ developer_data.py
@@ -1,5 +1,5 @@
 import streamlit as st
-col1, col2 = st.columns([0.7,0.5], gap="small", vertical_alignment="center")
+col1, col2 = st.columns([0.5,0.7], gap="small", vertical_alignment="center")
 with col1:
     st.image("Profile_img.png", width=400)
 
@@ -9,8 +9,6 @@ with col2:
         "Free Python Coder"
     )
 
-
-st.header("Dipesh Mani Tiwari")
 st.write("**Nationality:**  Indian")
 st.write("**Experinced:** Python ,Html")
 st.write("**Email:** @tiwaridipesh739@gmail.com")
